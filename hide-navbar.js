ed5ebf573a24bd51181d2c5265120afb455ddab3
BNOECHO <74729129+BNOECHO@users.noreply.github.com>
2022-06-23 15:10:10 +0800

    Revert "backend pull"

--- hide-navbar.js
+++ hide-navbar.js
@@ -3,10 +3,8 @@ window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-    document.getElementById("account-sidebar").style.top = "110px";
   } else {
     document.getElementById("navbar").style.top = "-85px";
-    document.getElementById("account-sidebar").style.top = "50px";
   }
   prevScrollpos = currentScrollPos;
 }
